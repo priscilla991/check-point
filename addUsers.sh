96a613a8f61f24a965a03f62e5b4451640c419be
priscilla991 <pmanedjingouana@gmail.com>
2025-04-13 09:10:12 +0200

    Add files via upload

--- addUsers.sh
+++ addUsers.sh
@@ -1,34 +1,38 @@
-#! /bin/bash 
+########################################################################
+#!/bin/bash
+#  version: 0.0
+#  Auteur: Priscilla
+#  Description: Script qui traite les utilisateurs
 
-#######################################################################################################################################
-# version 0.0
-# Priscilla
-# Description: Script qui créé automatiquement les utilisateurs
-#######################################################################################################################################
 
-#Vérifier si les arguments ont été passés 
 
-# Vérifier si des arguments ont été passés
+# Vérifions si un argument est deja passé
+
 if [ $# -eq 0 ]; then
-    echo "Il manque les noms d'utilisateurs en argument - Fin du script"
-    exit 1
+   echo "Il manque les noms d'utilisateurs en argument - Fin du script"
+   exit 1
 fi
 
-# Boucle pour traiter chaque utilisateur passé en argument
-for user in "$@"
-do
-    # Vérification de l'existence de l'utilisateur
-    if id "$user" &>/dev/null; then
-        echo "L'utilisateur $user existe déjà"
-    else
-        # Création de l'utilisateur avec sudo
-        sudo useradd "$user"
+#Vérification de l'existence des utilisateurs dans le système
 
-        # Vérification de la création de l'utilisateur
-        if [ $? -eq 0 ]; then
-            echo "L'utilisateur $user a été créé"
-        else
-            echo "Erreur à la création de l'utilisateur $user"
-        fi
-    fi
+for user1 in "$@" ; 
+do
+#Vérification de l'existence
+   if id "$user1" ; 
+   then
+      echo "L'utilisateur $user1 existe déjà"
+   else
+      echo "L'utilisateur $user1 n'existe pas encore."
+
+   #Création de l'utilisateur
+   sudo useradd "$user1"
+   
+#Vérification de la création d'utilisateur
+ if [ $? eq 0]; then
+   echo "L'utilisateur <user1> a été crée"
+else
+   echo "Erreur à la création de l'utilisateur $user1"
+ fi
+
+   fi
 done
